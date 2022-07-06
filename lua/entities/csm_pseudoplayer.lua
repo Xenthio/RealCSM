c10ad11c6ced9b9c5618b79aa3b825594ce56628
Xenthio <thegooster88@gmail.com>
2022-07-06 16:20:35 +1000

    Fix player shadow bodygroups

--- lua/entities/csm_pseudoplayer.lua
+++ lua/entities/csm_pseudoplayer.lua
@@ -115,6 +115,9 @@ function ENT:Think()
             self:SetCycle(LocalPlayer():GetCycle())
             self:SetSequence(LocalPlayer():GetSequence())
 
+            for k = 1, LocalPlayer():GetNumBodyGroups() do
+                self:SetBodygroup(k, LocalPlayer():GetBodygroup(k))
+            end
             --anims = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14}
             --for l, i in ipairs(anims) do
             for i = 0, 15 do
